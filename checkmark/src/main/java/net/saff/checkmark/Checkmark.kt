b0a800c7993a2aea0eaca01fc3ffb34df886e82f
David Saff <david@saff.net>
2024-05-08 19:46:08 -0400

    Add editorconfig

--- checkmark/src/main/java/net/saff/checkmark/Checkmark.kt
+++ checkmark/src/main/java/net/saff/checkmark/Checkmark.kt
@@ -23,162 +23,164 @@ import java.text.SimpleDateFormat
 import java.util.Date
 
 class Checkmark {
-  class Failure(s: String, e: Throwable? = null) :
-    AssertionError(e?.message?.let { m -> m + "\n" + s } ?: s, e) {
-    override fun getStackTrace(): Array<StackTraceElement> {
-      val causeHere = cause
-      return if (causeHere != null) {
-        causeHere.stackTrace
-      } else {
-        super.getStackTrace()
-      }
-    }
-
-    // IntelliJ + Gradle tends to print error messages twice, messing up the output.
-    // Therefore, only return the real error message once (yes, this could backfire, but we'll
-    // handle that when it comes)
-    var bigMessageRetrieved = false
-    override val message: String?
-      get() {
-        System.out.println("big message retrieved: $bigMessageRetrieved")
-        if (!bigMessageRetrieved || !suppressDuplicateMessages) {
-          bigMessageRetrieved = true
-          return super.message
+    class Failure(s: String, e: Throwable? = null) :
+        AssertionError(e?.message?.let { m -> m + "\n" + s } ?: s, e) {
+        override fun getStackTrace(): Array<StackTraceElement> {
+            val causeHere = cause
+            return if (causeHere != null) {
+                causeHere.stackTrace
+            } else {
+                super.getStackTrace()
+            }
         }
-        return "[duplicate message suppressed]"
-      }
 
-    override fun getLocalizedMessage(): String {
-      return super.message ?: super.getLocalizedMessage()
-    }
-  }
+        // IntelliJ + Gradle tends to print error messages twice, messing up the output.
+        // Therefore, only return the real error message once (yes, this could backfire, but we'll
+        // handle that when it comes)
+        var bigMessageRetrieved = false
+        override val message: String?
+            get() {
+                System.out.println("big message retrieved: $bigMessageRetrieved")
+                if (!bigMessageRetrieved || !suppressDuplicateMessages) {
+                    bigMessageRetrieved = true
+                    return super.message
+                }
+                return "[duplicate message suppressed]"
+            }
 
-  private val marks = mutableListOf<() -> String>()
-
-  fun <T> mark(note: T): T {
-    marks.add { note.toString() }
-    return note
-  }
-
-  fun mark(fn: () -> String) {
-    marks.add(fn)
-  }
-
-  private fun marks() = marks.joinToString { "\n  mark: ${it()}" }
-
-  companion object {
-    fun fail(s: String, e: Throwable? = null): Nothing = throw Failure(s, e)
-
-    fun <T> T.check(eval: Checkmark.(T) -> Boolean): T {
-      val cm = Checkmark()
-      val result = try {
-        cm.eval(this)
-      } catch (t: Throwable) {
-        fail("ERROR: ${allDebugOutput(this, cm, eval)}", t)
-      }
-      if (!result) {
-        fail("Failed assertion: ${allDebugOutput(this, cm, eval)}")
-      }
-      return this
-    }
-
-    fun checks(fn: () -> Unit) {
-      try {
-        fn()
-      } catch (e: Throwable) {
-        fail(extractClosureFields(fn).cleanPairsForDisplay(), e)
-      }
-    }
-
-    private fun extractClosureFields(closure: Any) = buildList {
-      closure::class.java.declaredFields.forEach { field ->
-        // Not sure why this is needed.
-        // https://github.com/dsaff/checkmark/issues/1
-        if (field.name != "INSTANCE") {
-          field.isAccessible = true
-          val gotten = field.get(closure)
-          if (!(gotten is Function<*>)) {
-            add(field.name.removePrefix("\$") to gotten)
-          }
+        override fun getLocalizedMessage(): String {
+            return super.message ?: super.getLocalizedMessage()
         }
-      }
     }
 
-    private fun <T> allDebugOutput(
-      receiver: T, cm: Checkmark, eval: Checkmark.(T) -> Boolean
-    ): String {
-      val reports = buildList {
-        add("actual" to receiver)
-        addAll(extractClosureFields(eval))
-        addAll(cm.marks.map { "marked" to it() })
-      }
+    private val marks = mutableListOf<() -> String>()
 
-      if (useJson) {
-        // SAFF: indentation (follow https://developer.android.com/kotlin/style-guide)
-        // SAFF: should this have a special case for only actual, like below?
-          // SAFF: indentation is annoying here
-        // SAFF: not all values are going to be strings, are they?
-          val jsonObject = JsonObject(reports.associate { it.first to JsonPrimitive(it.second.toString()) })
-        val dateString = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
-        val file = File("/tmp/compare_$dateString.json")
-        // Create a uniquely named file with a timestamp in the filename in /tmp
-
-
-        // SAFF: something.json is not always the right value
-          file.writeText(jsonObject.toString())
-        return "[more: $file]"
-      }
-
-      // SAFF: match all of this with above
-      return if (reports.size == 1) {
-        reports[0].second.toString().forCleanDisplay()
-      } else {
-        cleanPairsForDisplay(reports)
-      }
+    fun <T> mark(note: T): T {
+        marks.add { note.toString() }
+        return note
     }
 
-    private fun cleanPairsForDisplay(reports: List<Pair<String, Any?>>) =
-      reports.joinToString("") { "\n- ${it.first}: ${it.second.toString().forCleanDisplay()}" }
-
-    private fun String.forCleanDisplay(): String {
-      return if (!contains("\n")) {
-        this
-      } else {
-        val margin = "  |"
-        "\n$margin${replace("\n", "\n$margin")}"
-      }
+    fun mark(fn: () -> String) {
+        marks.add(fn)
     }
 
-    fun <T> T.checkCompletes(eval: Checkmark.(T) -> Unit): T {
-      val cm = Checkmark()
-      try {
-        cm.eval(this)
-      } catch (e: Throwable) {
-        throw Exception("Failed: <<$this>>${cm.marks()}", e)
-      }
-      return this
-    }
+    private fun marks() = marks.joinToString { "\n  mark: ${it()}" }
 
-      // SAFF: choose an indentation style
-    fun <T> useJson(fn: () -> T): T {
-      useJson = true
-      try {
-        return fn()
-      } finally {
-        useJson = false
-      }
-    }
+    companion object {
+        fun fail(s: String, e: Throwable? = null): Nothing = throw Failure(s, e)
 
-    private var useJson = false
-    var suppressDuplicateMessages = true
-  }
+        fun <T> T.check(eval: Checkmark.(T) -> Boolean): T {
+            val cm = Checkmark()
+            val result = try {
+                cm.eval(this)
+            } catch (t: Throwable) {
+                fail("ERROR: ${allDebugOutput(this, cm, eval)}", t)
+            }
+            if (!result) {
+                fail("Failed assertion: ${allDebugOutput(this, cm, eval)}")
+            }
+            return this
+        }
+
+        fun checks(fn: () -> Unit) {
+            try {
+                fn()
+            } catch (e: Throwable) {
+                fail(extractClosureFields(fn).cleanPairsForDisplay(), e)
+            }
+        }
+
+        private fun extractClosureFields(closure: Any) = buildList {
+            closure::class.java.declaredFields.forEach { field ->
+                // Not sure why this is needed.
+                // https://github.com/dsaff/checkmark/issues/1
+                if (field.name != "INSTANCE") {
+                    field.isAccessible = true
+                    val gotten = field.get(closure)
+                    if (!(gotten is Function<*>)) {
+                        add(field.name.removePrefix("\$") to gotten)
+                    }
+                }
+            }
+        }
+
+        private fun <T> allDebugOutput(
+            receiver: T, cm: Checkmark, eval: Checkmark.(T) -> Boolean
+        ): String {
+            val reports = buildList {
+                add("actual" to receiver)
+                addAll(extractClosureFields(eval))
+                addAll(cm.marks.map { "marked" to it() })
+            }
+
+            if (useJson) {
+                // SAFF: indentation (follow https://developer.android.com/kotlin/style-guide)
+                // SAFF: should this have a special case for only actual, like below?
+                // SAFF: indentation is annoying here
+                // SAFF: not all values are going to be strings, are they?
+                val jsonObject =
+                    JsonObject(reports.associate { it.first to JsonPrimitive(it.second.toString()) })
+                val dateString = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+                val file = File("/tmp/compare_$dateString.json")
+                // Create a uniquely named file with a timestamp in the filename in /tmp
+
+                // SAFF: something.json is not always the right value
+                file.writeText(jsonObject.toString())
+                return "[more: $file]"
+            }
+
+            // SAFF: match all of this with above
+            return if (reports.size == 1) {
+                reports[0].second.toString().forCleanDisplay()
+            } else {
+                cleanPairsForDisplay(reports)
+            }
+        }
+
+        private fun cleanPairsForDisplay(reports: List<Pair<String, Any?>>) =
+            reports.joinToString(separator = "") {
+                "\n- ${it.first}: ${it.second.toString().forCleanDisplay()}"
+            }
+
+        private fun String.forCleanDisplay(): String {
+            return if (!contains("\n")) {
+                this
+            } else {
+                val margin = "  |"
+                "\n$margin${replace("\n", "\n$margin")}"
+            }
+        }
+
+        fun <T> T.checkCompletes(eval: Checkmark.(T) -> Unit): T {
+            val cm = Checkmark()
+            try {
+                cm.eval(this)
+            } catch (e: Throwable) {
+                throw Exception("Failed: <<$this>>${cm.marks()}", e)
+            }
+            return this
+        }
+
+        // SAFF: choose an indentation style
+        fun <T> useJson(fn: () -> T): T {
+            useJson = true
+            try {
+                return fn()
+            } finally {
+                useJson = false
+            }
+        }
+
+        private var useJson = false
+        var suppressDuplicateMessages = true
+    }
 }
 
 fun thrown(fn: () -> Any?): Throwable? {
-  try {
-    fn()
-  } catch (t: Throwable) {
-    return t
-  }
-  return null
+    try {
+        fn()
+    } catch (t: Throwable) {
+        return t
+    }
+    return null
 }
--- checkmark/src/test/java/test/net/saff/checkmark/CheckmarkTest.kt
+++ checkmark/src/test/java/test/net/saff/checkmark/CheckmarkTest.kt
@@ -19,109 +19,111 @@ import java.io.File
 val checkmarkMarker = java.lang.AssertionError()
 
 class CheckmarkTest {
-  @Test
-  fun dontPrintFunctions() {
-    val fn = { "abc" }
-    thrown { checks { "def".check { it == fn() } } }!!.message!!.check { !it.contains("->") }
-  }
+    @Test
+    fun dontPrintFunctions() {
+        val fn = { "abc" }
+        thrown { checks { "def".check { it == fn() } } }!!.message!!.check { !it.contains("->") }
+    }
 
-  @Test
-  fun checkIncludesCaptures() {
-    val n = "Apple sauce"
-    thrown { "Pear soup".check { it == n } }!!.message.check { it!!.contains("Apple") }
-  }
+    @Test
+    fun checkIncludesCaptures() {
+        val n = "Apple sauce"
+        thrown { "Pear soup".check { it == n } }!!.message.check { it!!.contains("Apple") }
+    }
 
-  @Test
-  fun outputWithMark() {
-    val expect = """
+    @Test
+    fun outputWithMark() {
+        val expect = """
         |Failed assertion: 
         |- actual: A
         |- marked: B
     """.trimMargin().showWhitespace()
-    thrown { "A".check { it == mark("B") } }!!.message!!.showWhitespace().check {
-      it == expect
+        thrown { "A".check { it == mark("B") } }!!.message!!.showWhitespace().check {
+            it == expect
+        }
     }
-  }
 
 
-  @Test
-  fun onlyActual() {
-    // SAFF: DUP above?
-    val expect = "Failed assertion: A".trimMargin().showWhitespace()
-    thrown { "A".check { it == "B" } }!!.message!!.showWhitespace().check { it == expect }
-  }
+    @Test
+    fun onlyActual() {
+        // SAFF: DUP above?
+        val expect = "Failed assertion: A".trimMargin().showWhitespace()
+        thrown { "A".check { it == "B" } }!!.message!!.showWhitespace().check { it == expect }
+    }
 
 
-  @Test
-  fun jsonOutputWithMark() {
-    val message = Checkmark.useJson { thrown { "A".check { it == mark("B") } }!!.message!! }
-
-    val matcher = "\\[more: (.*\\.json)]".toPattern().matcher(message)
-    val jsonFile = matcher.check {
-      // SAFF: this outputs both marked and message.  Only one is needed
-      mark(message)
-      it.find()
-    }.group(1) ?: fail(message)
-    jsonFile.check { it.startsWith("/tmp") }
-    val element = Json.parseToJsonElement(File(jsonFile).readText())
-    // SAFF: DUP?
-    element.jsonObject.get("actual").check { it?.jsonPrimitive?.content == "A" }
-    element.jsonObject.get("marked").check { it?.jsonPrimitive?.content == "B" }
-  }
-
-
-  @Test
-  fun jsonOutputWithMarkTheory() {
-    // SAFF: DUP above
-    theory(exploreTreeFates(1)) {
-      val expectedActual = chooseString("actual?")
-      val expectedMarked = chooseString("marked?")
-      if (expectedActual != expectedMarked) {
-        val message =
-          Checkmark.useJson { thrown { expectedActual.check { it == mark(expectedMarked) } }!!.message!! }
+    @Test
+    fun jsonOutputWithMark() {
+        val message = Checkmark.useJson { thrown { "A".check { it == mark("B") } }!!.message!! }
 
         val matcher = "\\[more: (.*\\.json)]".toPattern().matcher(message)
         val jsonFile = matcher.check {
-          // SAFF: this outputs both marked and message.  Only one is needed
-          mark(message)
-          it.find()
+            // SAFF: this outputs both marked and message.  Only one is needed
+            mark(message)
+            it.find()
         }.group(1) ?: fail(message)
+        jsonFile.check { it.startsWith("/tmp") }
         val element = Json.parseToJsonElement(File(jsonFile).readText())
         // SAFF: DUP?
-        element.jsonObject.get("actual").check { it?.jsonPrimitive?.content == expectedActual }
-        element.jsonObject.get("marked").check { it?.jsonPrimitive?.content == expectedMarked }
-      }
+        element.jsonObject.get("actual").check { it?.jsonPrimitive?.content == "A" }
+        element.jsonObject.get("marked").check { it?.jsonPrimitive?.content == "B" }
     }
-  }
 
-  @Test
-  fun meldStackTrace() {
-    var rootStackTrace: List<StackTraceElement>? = null
-    thrown {
-      checks {
-        val re = RuntimeException("flubber")
-        rootStackTrace = re.stackTrace.toList()
-        throw re
-      }
-    }!!.stackTrace.toList().check { trace -> trace[0] == rootStackTrace!![0] }
-  }
 
-  @Test
-  fun checkCompletesWorks() {
-    thrown {
-      checkCompletes {
-        mark("abc")
-        throw RuntimeException("foo")
-      }
-    }!!.message!!.check { it.contains("abc") }
-  }
+    @Test
+    fun jsonOutputWithMarkTheory() {
+        // SAFF: DUP above
+        theory(exploreTreeFates(1)) {
+            val expectedActual = chooseString("actual?")
+            val expectedMarked = chooseString("marked?")
+            if (expectedActual != expectedMarked) {
+                val message =
+                    Checkmark.useJson { thrown { expectedActual.check { it == mark(expectedMarked) } }!!.message!! }
 
-  @Test
-  fun onlyReturnMessageOnce() {
-    val n = "Apple sauce"
-    thrown { "Pear soup".check { it == n } }!!.let { error ->
-      error.message!!.check { it.contains("Apple") }
-      error.message!!.check { it == "[duplicate message suppressed]" }
+                val matcher = "\\[more: (.*\\.json)]".toPattern().matcher(message)
+                val jsonFile = matcher.check {
+                    // SAFF: this outputs both marked and message.  Only one is needed
+                    mark(message)
+                    it.find()
+                }.group(1) ?: fail(message)
+                val element = Json.parseToJsonElement(File(jsonFile).readText())
+                // SAFF: DUP?
+                element.jsonObject.get("actual")
+                    .check { it?.jsonPrimitive?.content == expectedActual }
+                element.jsonObject.get("marked")
+                    .check { it?.jsonPrimitive?.content == expectedMarked }
+            }
+        }
     }
-  }
+
+    @Test
+    fun meldStackTrace() {
+        var rootStackTrace: List<StackTraceElement>? = null
+        thrown {
+            checks {
+                val re = RuntimeException("flubber")
+                rootStackTrace = re.stackTrace.toList()
+                throw re
+            }
+        }!!.stackTrace.toList().check { trace -> trace[0] == rootStackTrace!![0] }
+    }
+
+    @Test
+    fun checkCompletesWorks() {
+        thrown {
+            checkCompletes {
+                mark("abc")
+                throw RuntimeException("foo")
+            }
+        }!!.message!!.check { it.contains("abc") }
+    }
+
+    @Test
+    fun onlyReturnMessageOnce() {
+        val n = "Apple sauce"
+        thrown { "Pear soup".check { it == n } }!!.let { error ->
+            error.message!!.check { it.contains("Apple") }
+            error.message!!.check { it == "[duplicate message suppressed]" }
+        }
+    }
 }
