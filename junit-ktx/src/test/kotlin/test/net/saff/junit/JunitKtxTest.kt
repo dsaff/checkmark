7c554afc06b12b2d27d0e9d12fb89ee3023f289d
David Saff <david@saff.net>
2025-07-23 16:40:41 -0400

    rings

--- junit-ktx/src/test/kotlin/test/net/saff/junit/JunitKtxTest.kt
+++ junit-ktx/src/test/kotlin/test/net/saff/junit/JunitKtxTest.kt
@@ -1,13 +1,19 @@
 package test.net.saff.junit
 
 import net.saff.checkmark.Checkmark.Companion.check
+import net.saff.checkmark.thrown
+import net.saff.junit.ring
+import net.saff.junit.rings
 import net.saff.junit.wrap
+import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.rules.TestRule
 import org.junit.runners.model.Statement
+import kotlin.coroutines.resume
 
 class JunitKtxTest {
-    @Test fun testWrap() {
+    @Test
+    fun testWrap() {
         buildList {
             val rule = TestRule { base, _ ->
                 object : Statement() {
@@ -21,4 +27,88 @@ class JunitKtxTest {
             rule.wrap { add("during") }
         }.check { it == listOf("before", "during", "after") }
     }
+
+    @Test
+    fun ringsEverythingGood() {
+        var status = "starting"
+        rings {
+            status.check { it == "starting" }
+            val three = ring {
+                status = "in ring"
+                resume(3)
+                status = "after resume"
+            }
+            three.check { it == 3 }
+            status.check { it == "in ring" }
+        }
+        status.check { it == "after resume" }
+    }
+
+    @Test
+    fun ringsThereIsAFailure() {
+        var status = "starting"
+        thrown {
+            rings {
+                status.check { it == "starting" }
+                val three = ring {
+                    status = "in ring"
+                    try {
+                        resume(3)
+                    } finally {
+                        status = "after resume"
+                    }
+                }
+                three.check { it == 4 }
+                status.check { it == "in ring" }
+            }
+        }.check { it!!.message!!.contains("assertion: 3") }
+        status.check { it == "after resume" }
+    }
+
+    @Test
+    fun ringsRethrow() {
+        var status = "starting"
+        thrown {
+            rings {
+                status.check { it == "starting" }
+                val three = ring {
+                    status = "in ring"
+                    try {
+                        resume(3)
+                    } catch (t: Throwable) {
+                        throw RuntimeException("something different!")
+                    } finally {
+                        status = "after resume"
+                    }
+                }
+                three.check { it == 4 }
+                status.check { it == "in ring" }
+            }
+        }.check { it!!.message!!.contains("different") }
+        status.check { it == "after resume" }
+    }
+
+    @Test
+    fun ringsRethrowWithCause() {
+        val e = thrown {
+            rings {
+                val three = ring {
+                    try {
+                        resume(3)
+                    } catch (t: Throwable) {
+                        throw RuntimeException("something different!", t)
+                    }
+                }
+                three.check { it == 4 }
+            }
+        }
+        e.check { it!!.message!!.contains("different") }
+    }
+
+    // SAFF: so it turns out, gradle strips the outer exception, if the inner one is an Assert.
+    // Does this matter for rings?  Maybe, if we're trying to add data
+    // an AssertionError.  https://github.com/gradle/gradle/pull/26399/files
+    // https://github.com/gradle/gradle/issues/26299
+    //
+    // Do I want to do something about this?
 }
