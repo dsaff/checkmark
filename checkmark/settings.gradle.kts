2678c61f51cf3621a99ab5669934c9236906b8b0
David Saff <david@saff.net>
2026-03-13 15:24:55 -0400

    Create settings.gradle.kts

---
+++ checkmark/settings.gradle.kts
@@ -0,0 +1,31 @@
+rootProject.name = "sanctuary"
+
+// SAFF: DUP?
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+    }
+}
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+    plugins {
+        kotlin("jvm") version "2.1.10"
+        kotlin("multiplatform") version "2.1.10"
+        kotlin("plugin.serialization") version "2.1.10"
+    }
+}
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
+
+include(":prettyprint")
+project(":prettyprint").projectDir = file("../prettyprint")
+
+include(":befuzz")
+project(":befuzz").projectDir = file("../../befuzz")
